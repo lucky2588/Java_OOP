378139d6038731d40a34e196708ca25541d10b25
lucky2588 <duwcsthang2001@gmail.com>
2022-10-14 18:13:24 +0700

    Create Idol.java

---
+++ ArrayList/B1/src/Model/Idol.java
@@ -0,0 +1,44 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Idol {
+    private String fullName;
+    private String email;
+    private ArrayList<Follower> followers;
+
+    public Idol(String fullName, String email, ArrayList<Follower> followers) {
+        this.fullName = fullName;
+        this.email = email;
+        this.followers = followers;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Follower> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(ArrayList<Follower> followers) {
+        this.followers = followers;
+    }
+
+    @Override
+    public String toString() {
+        return " -Idol Name:"+fullName+" -Email"+email+" Followers:\n"+followers+"\n";
+    }
+}
