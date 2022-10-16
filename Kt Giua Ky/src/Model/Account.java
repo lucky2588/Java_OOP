b1b521d8bde03b67c35f0fca33fab62c014b5093
lucky2588 <duwcsthang2001@gmail.com>
2022-10-16 12:11:34 +0700

    Update Account.java

--- Kt Giua Ky/src/Model/Account.java
+++ Kt Giua Ky/src/Model/Account.java
@@ -37,4 +37,9 @@ public class Account {
     public void setEmail(String email){
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "\t-ID:"+getUserName()+"\t-Pass: "+getPassWord()+"";
+    }
 }
