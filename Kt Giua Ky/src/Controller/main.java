4c21704bb6b97b7c48e04d4fee6d48533ff8d6d9
lucky2588 <duwcsthang2001@gmail.com>
2022-10-16 12:11:25 +0700

    Update main.java

--- Kt Giua Ky/src/Controller/main.java
+++ Kt Giua Ky/src/Controller/main.java
@@ -1,29 +1,30 @@
 package Controller;
-
 import Model.Account;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
 public class main {
     public static void main(String[] args) {
         Pattern checkPass = Pattern.compile("^[0-9A-Za-z.]{7,15}+$");
-        Pattern checkID = Pattern.compile("[a-zA-z0-9]{3,}+$");
-        Pattern checkEmail = Pattern.compile("^[a-zA-z0-9 .@]{3,}+$");
+        Pattern checkID = Pattern.compile("[a-zA-Z0-9]{3,}+$");
+        Pattern checkEmail = Pattern.compile("^[a-zA-z0-9.@]{3,}+$");
         // Khởi tạo 1 list chứa các Account
         ArrayList<Account> listAccount = new ArrayList<>();
         Account user = new Account("user1", "Aa123456.", "user@gmail.com");
+        Account user1 = new Account("user2", "Aa123456.", "user1@gmail.com");
         listAccount.add(user);
+        listAccount.add(user1);
         Scanner sc = new Scanner(System.in);
         Boolean checkOne = true;
-        Boolean checkTwo = true;
-        Boolean checkThree = true;
         int checkNumber = 0;
         do {
             //Viết chương trình thực hiện:
             // 1. Đăng nhập , 2. Đăng Kí
+            System.out.println("List Now");
+            for (int i = 0; i < listAccount.size(); i++) {
+                System.out.println("-"+listAccount.get(i).toString());
+            }
             System.out.println("==========Welcome to the endless plains==========");
             System.out.printf(" \n 1.Log in \t 2.Register an account \n");
             System.out.println("\n \n Choose : ");
@@ -36,18 +37,18 @@ public class main {
                     String enterAcc = sc.nextLine();
                     System.out.println("import Password ");
                     String passWord = sc.nextLine();
-                    for (int i = 0; i < listAccount.size(); i++) {
-                        if (listAccount.get(i).getUserName().compareTo(enterAcc) == 0 && listAccount.get(i).getPassWord().compareTo(passWord) == 0) {
+                    for (int i = 0; i < listAccount.size(); i++){
+                        if(enterAcc.equals(listAccount.get(i).getUserName()) && passWord.equals(listAccount.get(i).getPassWord())){
                             System.out.println(" Wait for a second....... ");
                             System.out.println("Logged in successfully !!!");
                             System.out.println("Welcome to " + listAccount.get(i).getUserName() + "\n");
-                            System.out.println("==========Your request=========");
-                            System.out.println("1.change Username ");
-                            System.out.println("2.Change Email ");
+                            System.out.println("==========Your Request=========");
+                            System.out.println("1.Change Username");
+                            System.out.println("2.Change Email");
                             System.out.println("3.Change PassWd");
-                            System.out.println("4.Log Out ");
-                            System.out.println("0. Exit Program ");
-                            System.out.println("\n Your Decision  ");
+                            System.out.println("4.Log Out");
+                            System.out.println("0.Exit Program");
+                            System.out.println("=>> Your Decision:");
                             int decision = sc.nextInt();
                             sc.nextLine();
                             endPro:
@@ -56,11 +57,11 @@ public class main {
                                     System.out.println("Import Username new");
                                     String usernameNew = sc.nextLine();
                                     if (checkID.matcher(usernameNew).find()) {
-                                        for (int j = 0; j < listAccount.size(); j++) {
-                                            if (usernameNew.compareToIgnoreCase(listAccount.get(i).getUserName()) != 0) {
+                                        for (int j = 0; j < listAccount.size(); j++){
+                                            if (usernameNew.compareToIgnoreCase(listAccount.get(i).getUserName())!= 0){
                                                 listAccount.get(i).setUserName(usernameNew);
                                                 System.out.println("Set Seccuress");
-                                            } else {
+                                            }else{
                                                 System.out.println("username already exists , please import Username diffirent ");
                                                 decision = 1;
                                                 break; // Chưa xử lí được vòng cho nhập lại Username
@@ -107,7 +108,10 @@ public class main {
                                     System.out.println("Import numbers resquet ^^");
                                     break;
                             }
-                        } else {
+                            break;
+                        }else{
+                            if(i < listAccount.size()-1) // để xét 1 lần
+                                continue;
                             System.out.println("Log in fail !!! Please Check ID and PassWord that you enter ");// Xử lí chức năng Đăng nhập lại và Quên Mật Khẩu
                             System.out.println("==>> Suggestions ");
                             System.out.println("1.login again \t 2.Forgot Password \t 3.Out Program ");
@@ -117,21 +121,22 @@ public class main {
                             switch (decisionTwo) {
                                 case 1:
                                     Choose = 1;
-                                    break;
+                                    break; // Xử Lí Thành Công
                                 case 2:
                                     // Xử lí phần Quên mk , Nhập Email đã đăng kí
                                     System.out.println("Import email You Registered ... ");
                                     String checkEmail1 = sc.nextLine();
-                                    for (int j = 0; j < listAccount.size(); j++) {
-                                        if (checkEmail1.equals(listAccount.get(i).getEmail())) { // Check xem email trùng với email của User nào trong list
+                                   endPro1: for(int j = 0; j < listAccount.size(); j++){
+                                        if (checkEmail1.equals(listAccount.get(i).getEmail())){ // Check xem email trùng với email của User nào trong list
                                             System.out.println("Are you " + listAccount.get(i).getUserName());
                                             System.out.println("Import PassWD new ");
                                             String passWDnew = sc.nextLine();
                                             listAccount.get(i).setPassWord(passWDnew);
                                             System.out.println("Pass Wd set Vail !!");
-                                        } else {
+                                            break endPro1;
+                                        }else{
                                             System.out.println("Email not found , please Input Once  !!!");
-                                            Choose = 1; // Chưa back về được lệnh nhập lại Email
+                                            decisionTwo = 2; // Chưa back về được lệnh nhập lại Email
                                         }
                                         break;
                                         // Xử lí xong phần Quên Mật Khẩu
@@ -149,9 +154,33 @@ public class main {
                     break;
                 //Case 2 này xử lí phần đăng kí tài khoản ..........
                 case 2: // Xử Lí From Đăng Kí
+                    System.out.println("Import Email  ");
+                    String emailNew = sc.nextLine();
+                    System.out.println("Import Usernew");
+                    String usernameNew = sc.nextLine();
+                    System.out.println("Import PassWd");
+                    String passwdNew = sc.nextLine();
+                    if (checkID.matcher(usernameNew).find() && checkEmail.matcher(emailNew).find()) { // check Ký tự và yêu cầu về tk và email
+                        for (int i = 0; i < listAccount.size(); i++){
+                            if (usernameNew.equals(listAccount.get(i).getUserName()) && emailNew.equals(listAccount.get(i).getEmail())) {
+                                System.out.println("Username or Email already exist ");
+                                System.out.println("Import once ");
+                                break;
+                                // chưa cho trở về vòng lặp nhập lại
+                            } else {
+                                // Khởi tạo đối tượng và thêm Account new
+                                Account user2 = new Account(usernameNew, passwdNew, emailNew);
+                                listAccount.add(user2);
+                                System.out.println("Create new User successful .. Please come back and login");
+                                break;
+                            }
+                        }
+                    } else {
+                        System.out.println("Username or Email , PassWD not Vail , Note Char and String have with special Signature ");
+                    }
+                    // Phần cuối vòng lặp
             }
-        }while(checkOne);
-
+        } while (checkOne);
         System.out.println("End Program");
     }
 }
